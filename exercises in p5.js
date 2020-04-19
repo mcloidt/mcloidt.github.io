5b4dd0c49cda388f2675b4b9873fd3ea61a22a4a
mcloidt <62402477+mcloidt@users.noreply.github.com>
2020-04-19 12:28:33 -0400

    Update exercises in p5.js

--- exercises in p5.js
+++ exercises in p5.js
@@ -4,3 +4,11 @@
     <meta charset="utf-8">
     <link href="exercises in p5.js.css" rel="stylesheet" type="text/css">
     <title>exercises in p5.js</title>
+<script src="p5.js"></script>
+    <script src="p5.sound.min.js"></script>
+<iframe src="https://editor.p5js.org/mcloidt/embed/nSGzVi7N"></iframe>
+<iframe src="https://editor.p5js.org/mcloidt/embed/OBzki-s7"></iframe>
+    <body>
+
+    </body>
+    </html>
